61cb9c3da5e8a1d14196f3eac4264771e4749aab
leoneldavidalvarez1-dot <leodavidalvarez494@gmail.com>
2026-06-02 10:58:46 -0500

    Feat: Fase roja de la funcionalidad Registrar un Producto

--- ProyectoInventario/Inventario.Test.cs
+++ ProyectoInventario/Inventario.Test.cs
@@ -1,11 +1,21 @@
-﻿namespace ProyectoInventario
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Inventario.Tests
 {
     [TestClass]
-    public sealed class Test1
+    public class InventarioTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RegistrarProducto()
         {
+            var inventario = new InventarioService();
+
+            inventario.RegistrarProducto(
+                new Producto("P001", "Mouse", 10));
+
+            Assert.AreEqual(
+                1,
+                inventario.ListarProductos().Count);
         }
     }
 }
